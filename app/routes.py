6d39871eb7d4549f39ff2c6f4d825a2acddd5899
Tiffany Bissonnette <tiffanybissonnette@Tiffanys-Air.wifi.gsu.edu>
2023-03-26 23:45:14 -0400

    login and register

---
+++ app/routes.py
@@ -0,0 +1,16 @@
+
+from app import app
+
+from flask import render_template, request, url_for, redirect
+from flask_login import current_user, login_user, logout_user
+
+from .auth.forms import SignUpForm, LoginForm
+from .models import User
+
+@app.route('/')
+def homePage():
+    return render_template('index.html')
+
+
+
+
---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+class Config():
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
+    # FLASK_ENV = os.environ.get('FLASK_ENV')  --> just to show the soon-to-be-deprecated example
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
